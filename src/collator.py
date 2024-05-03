save_hparams(to load checkpoint) low precision torch fix bug

src/collator.py
@@ -16,6 +16,6 @@ class NERDataCollator(DataCollatorWithPadding):
 
         labels = [feature['labels'] for feature in features]
         labels = [torch.tensor([LABEL_TO_IDX[label] for label in batch_label]) for batch_label in labels]
-        labels = pad_sequence(labels, padding_value=LABEL_TO_IDX[PAD_LABEL], batch_first=True)
+        labels = pad_sequence(labels, padding_value=0, batch_first=True)
         # import pdb; pdb.set_trace()
         return {'input_ids': batch['input_ids'], 'attention_mask': batch['attention_mask'], 'word_ids': word_ids, 'labels': labels}

train.py
@@ -12,6 +12,9 @@ from src.variable import LABEL_TO_IDX, PAD_LABEL
 
 builder.has_sufficient_disk_space = lambda needed_bytes, directory=".": True
 
+import torch
+torch.set_float32_matmul_precision('medium' | 'high')
+
 def print_hparams(hparams):
     # haparams: Namespace
     print("Hyperparameters:")
@@ -79,8 +82,8 @@ def main(hparams):
 
     print("Training model...")
     collator = NERDataCollator(tokenizer=tokenizer)
-    train_loader = DataLoader(train_dataset, collate_fn=collator, batch_size=hparams.batch_size)
-    val_loader = DataLoader(val_dataset, collate_fn=collator, batch_size=hparams.batch_size)
+    train_loader = DataLoader(train_dataset, collate_fn=collator, batch_size=hparams.batch_size, num_workers=47)
+    val_loader = DataLoader(val_dataset, collate_fn=collator, batch_size=hparams.batch_size, num_workers=47)
     logger = TensorBoardLogger("tb_logs", name=get_run_name(hparams))
     trainer = Trainer(max_epochs=hparams.epoch, fast_dev_run=hparams.fast_dev_run, logger=logger)
     trainer.fit(model, train_loader, val_loader)